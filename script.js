0cd6ffd8fbcd6de22f09a7d81160f418628d8961
Jesse Cooper <Elendel19@gmail.com>
2022-07-31 18:25:05 -0700

    Finish logic for math operations and negatives

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const display = document.querySelector('.display')
-const buttons = document.querySelectorAll('.button')
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.button');
 
-let total = 0
-let currentNum = 0
-let op = ''
-display.textContent = currentNum
-
+let total = 0;
+let currentNum = 0;
+let op = '';
+display.textContent = currentNum;
 
 buttons.forEach(button => button.addEventListener('mouseenter', () => {
     button.style.border = '2px #d3d3d3 solid'
@@ -13,26 +12,30 @@ buttons.forEach(button => button.addEventListener('mouseenter', () => {
 
 buttons.forEach(button => button.addEventListener('mouseleave', () => {
     button.style.border = null
-}))
+}));
 
 buttons.forEach(button => button.addEventListener('click', () => {
     button.style.filter = 'brightness(150%)'
     setTimeout(() => {
         button.style.filter = null
     }, 100);
-}))
+}));
 
 buttons.forEach(button => button.addEventListener('click', (e) => {
     
     if (isNaN(parseFloat(e.target.id))) {
         if (e.target.id == 'decimal' && !/\./.test(currentNum)) {
          currentNum += "."
+    } else if (e.target.id == 'decimal' && /\./.test(currentNum)) {
+        return
     } else if (e.target.id == 'clear') {
         total = 0
         currentNum = 0
         display.textContent = currentNum
-    }else if (/\./.test(currentNum)) {
-        return 
+    } else if (e.target.id == 'negative') {
+        let temp = parseFloat(currentNum)
+        parseFloat(currentNum) > 0 ? currentNum = Math.abs(temp) * -1 : currentNum = Math.abs(temp);
+        return display.textContent = currentNum
     } else {
         if (total == 0) {
         total = parseFloat(currentNum)
@@ -44,45 +47,35 @@ buttons.forEach(button => button.addEventListener('click', (e) => {
         operate(op, total, currentNum);
         currentNum = 0
         op = e.target.id;
-        if (total.toString().length > 8) {
+        if (total.toString().length > 10) {
             display.textContent =  total.toExponential(4)
         } else {
             display.textContent = total
-        }
-        
-    }
+        }};
         
     } else {
     currentNum === 0 ? currentNum = e.target.id : currentNum += e.target.id;
     
-    if (currentNum.length > 8) {
+    if (currentNum.length > 10) {
         display.textContent = parseFloat(currentNum).toExponential(4)
     }else {
         display.textContent = currentNum
-    }
-    
-   }
-    
-}))
+    }}}));
 
 function add(currentNum, nextNum) {
     total =  (currentNum + parseFloat(nextNum));
     display.textContent = total;
-    
 };
 
 function subtract(currentNum, nextNum) {
     total =  parseFloat(currentNum) - parseFloat(nextNum);
     display.textContent = total;
-    
 };
 
 function multiply(currentNum, nextNum) {
     total =  parseFloat(currentNum) * parseFloat(nextNum);
     display.textContent = total;
-    console.log(total)
 };
-
 
 function divide(currentNum, nextNum) {
     total =  parseFloat(currentNum) / parseFloat(nextNum);
